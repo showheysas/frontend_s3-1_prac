2c906e6cb276815b1de2a18a370df258b5c1c639
shoheysas <shoheysas@gmail.com>
2025-01-31 01:00:37 +0900

    \create\confirm\page.jsxを元に戻す

--- src/app/customers/create/confirm/page.jsx
+++ src/app/customers/create/confirm/page.jsx
@@ -5,21 +5,12 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Suspense } from "react"; //（デプロイ時エラー処理）
 
-export default function ConfirmPageWrapper() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ConfirmPage />
-    </Suspense>
-  )
-}
-
-
-function ConfirmPage() {
+// 元のコード
+export default function ConfirmPage() {
   const router = useRouter();
   const customer_id = useSearchParams().get("customer_id");
   const [customer, setCustomer] = useState(null);
 
-  // メインコンポーネントをSuspenseでラップ（デプロイ時エラー処理）
   useEffect(() => {
     const fetchAndSetCustomer = async () => {
       const customerData = await fetchCustomer(customer_id);
@@ -42,3 +33,41 @@ function ConfirmPage() {
     </>
   );
 }
+
+// メインコンポーネントをSuspenseでラップ（デプロイ時エラー処理）
+// export default function ConfirmPageWrapper() {
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <ConfirmPage />
+//     </Suspense>
+//   )
+// }
+
+
+// function ConfirmPage() {
+//   const router = useRouter();
+//   const customer_id = useSearchParams().get("customer_id");
+//   const [customer, setCustomer] = useState(null);
+
+//   useEffect(() => {
+//     const fetchAndSetCustomer = async () => {
+//       const customerData = await fetchCustomer(customer_id);
+//       setCustomer(customerData);
+//     };
+//     fetchAndSetCustomer();
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="card bordered bg-white border-blue-200 border-2 max-w-sm m-4">
+//         <div className="alert alert-success p-4 text-center">
+//           正常に作成しました
+//         </div>
+//         <OneCustomerInfoCard {...customer} />
+//         <button onClick={() => router.push("./../../customers")}>
+//           <div className="btn btn-primary m-4 text-2xl">戻る</div>
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
